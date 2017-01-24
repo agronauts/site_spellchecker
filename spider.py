9426831e8ed2264dc4e50587999ee864aac56e1a
Patrick Nicholls <patrickn@estaroffice.local>
2017-01-25 12:22:23 +1300

    Spider extracts links from initial page

--- spider.py
+++ spider.py
@@ -1,9 +1,16 @@
 import requests
+import bs4
 
 class Spider:
 
     def __init__(self, root_url):
         self._content = requests.get(root_url).content
+        self.links = self._extract_links(self._content)
+
+    def _extract_links(self, page):
+        soup = bs4.BeautifulSoup(page, 'html.parser')
+        links = [tag['href'] for tag in soup('a')]
+        return links
 
     def __iter__(self):
         yield self._content
--- test_spider.py
+++ test_spider.py
@@ -6,7 +6,13 @@ from spider import Spider
 website_url = 'http://localhost:99999'
 
 test_website_content = b'''<html>
-    <h1>Hello world</h1>
+<h1>Hello world</h1>
+<ul>
+    <li><a href="/feature/thing1">link1</a></li>
+    <li><a href="/feature/thing2">link2</a></li>
+    <li><a href="/feature/thing3">link3</a></li>
+</ul>
+    
 </html>'''
 
 @pytest.fixture(autouse=True)
@@ -14,10 +20,16 @@ def no_requests(monkeypatch):
     monkeypatch.setattr(requests, 'get', lambda x: requests.Response())
     monkeypatch.setattr(requests.Response, 'content', test_website_content)
 
-def test_gets_initial_page_contents(monkeypatch):
+def test_gets_initial_page_contents():
     spider = Spider(website_url)
 
     assert next(iter(spider)) == test_website_content
 
+def test_add_links_from_page_in_order():
+    test_website_links = ['/feature/thing%d' % i for i in range(1, 4)]
+
+    spider = Spider(website_url)
+
+    assert spider.links == test_website_links
     #Crawl local site
     #Run test server on testing
