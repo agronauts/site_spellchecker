ad7cdf1abe18319a1116e78fedfb23f48f96c4a2
Patrick Nicholls <patrickn@estaroffice.local>
2017-01-25 12:03:13 +1300

    Refactor tests to monkeypatch get requests

--- spider.py
+++ spider.py
@@ -3,10 +3,7 @@ import requests
 class Spider:
 
     def __init__(self, root_url):
-        self._content = self._get_content(root_url)
-
-    def _get_content(self, url):
-        return requests.get(url)
+        self._content = requests.get(root_url).content
 
     def __iter__(self):
         yield self._content
--- test_spider.py
+++ test_spider.py
@@ -1,21 +1,23 @@
 import pytest
 import requests
+
 from spider import Spider
 
 website_url = 'http://localhost:99999'
 
-def test_gets_initial_page_contents(monkeypatch):
-    def test_website_content(*args, **kwargs):
-        return b'''<html>
-            <h1>Hello world</h1>
-        </html>'''
-    website_content = test_website_content()
-    #website_content = requests.get(website_url).content
-    monkeypatch.setattr(requests, 'get', test_website_content)
+test_website_content = b'''<html>
+    <h1>Hello world</h1>
+</html>'''
 
+@pytest.fixture(autouse=True)
+def no_requests(monkeypatch):
+    monkeypatch.setattr(requests, 'get', lambda x: requests.Response())
+    monkeypatch.setattr(requests.Response, 'content', test_website_content)
+
+def test_gets_initial_page_contents(monkeypatch):
     spider = Spider(website_url)
 
-    assert next(iter(spider)) == website_content
+    assert next(iter(spider)) == test_website_content
 
     #Crawl local site
     #Run test server on testing
