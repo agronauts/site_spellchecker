d065cfb928b096936db5dadad72fb99676f695e4
Patrick Nicholls <patrickn@estaroffice.local>
2017-01-25 13:31:33 +1300

    Spider processes links from main page

--- spider.py
+++ spider.py
@@ -4,14 +4,18 @@ import bs4
 class Spider:
 
     def __init__(self, root_url):
+        self._root_url = root_url
         self._content = requests.get(root_url).content
-        print(self._content)
-        self.links = self._extract_links(self._content)
+        self.next_links = self._extract_links(self._content)
 
     def _extract_links(self, page):
         soup = bs4.BeautifulSoup(page, 'html.parser')
-        links = [tag['href'] for tag in soup('a')]
+        links = [self._root_url + tag['href'] for tag in soup('a')]
         return links
 
     def __iter__(self):
-        yield self._content
+        while self.next_links != []:
+            yield self._content
+            self._content = requests.get(self.next_links.pop(0)).content
+            self.next_links.extend(self._extract_links(self._content))
+
--- test_spider.py
+++ test_spider.py
@@ -4,10 +4,14 @@ import requests
 from spider import Spider
 from collections import OrderedDict
 
-website_url = 'http://localhost:99999/index'
+website_url = 'http://localhost:99999'
 
 test_website_content = OrderedDict()
-test_website_content['index'] = b'''<html>
+for i in range(1,4):
+    test_website_content['thing%d' % i] = b'''<html>
+    <h1>Chinatown%d</h1>
+    </html>''' % i
+test_website_content[''] = b'''<html>
 <h1>Hello world</h1>
 <ul>
     <li><a href="/thing1">link1</a></li>
@@ -15,10 +19,6 @@ test_website_content['index'] = b'''<html>
     <li><a href="/thing3">link3</a></li>
 </ul>
 </html>'''
-for i in range(1,4):
-    test_website_content['thing%d' % i] = b'''<html>
-    <h1>Chinatown%d</h1>
-    </html>''' % i
 
 
 @pytest.fixture(autouse=True)
@@ -35,19 +35,24 @@ def no_requests(monkeypatch):
 def test_gets_initial_page_contents():
     spider = Spider(website_url)
 
-    assert next(iter(spider)) == test_website_content['index']
+    assert next(iter(spider)) == test_website_content['']
 
 def test_add_links_from_page_in_order():
-    test_website_links = ['/thing%d' % i for i in range(1, 4)]
+    test_website_links = [website_url + '/thing%d' % i for i in range(1, 4)]
 
     spider = Spider(website_url)
 
-    assert spider.links == test_website_links
+    assert spider.next_links == test_website_links
 
-def test_process_all_pages():
+def test_process_links_in_initial_page():
+    expected_pages = [test_website_content['thing%d' % i] for i in range(1,4)]
+
     spider = Spider(website_url)
+    gen = iter(spider)
+    next(gen)
 
-    for spider_page, expected_page in zip(spider, test_website_content.values()):
+    assert len(spider.next_links) == 3
+    for spider_page, expected_page in zip(gen, expected_pages):
         assert spider_page == expected_page
     #Crawl local site
     #Run test server on testing
